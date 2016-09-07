57444164a02e97ab1be0f0c305b07669fe024c86
Maru Amallo-amamarul <ama_marul@hotmail.com>
2016-09-07 17:31:56 -0300

    Add Url Clicked Tracking
    Migration, Model and controller

---
+++ src/Model/SentEmailUrlClicked.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace jdavidbakr\MailTracker\Model;
+
+use Illuminate\Database\Eloquent\Model;
+use Model\SentEmail;
+
+class SentEmailUrlClicked extends Model
+{
+    protected $table = 'sent_emails_url_clicked';
+
+    protected $fillable = [
+    	'sent_email_id',
+    	'url',
+    	'clicks',
+    ];
+
+    public function email()
+    {
+      return $this->belongsTo(SentEmail::class,'sent_email_id');
+    }
+}
