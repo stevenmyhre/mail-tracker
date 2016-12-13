d849553fd81bc7dddc015a308cda3ce4fc660f62
Simon Depelchin <dep.simon@gmail.com>
2016-12-13 15:42:29 +0100

    Putting back the message id migration

--- src/MailTrackerServiceProvider.php
+++ src/MailTrackerServiceProvider.php
@@ -76,6 +76,9 @@ class MailTrackerServiceProvider extends ServiceProvider
             $this->publishes([
                 __DIR__.'/../migrations/2016_09_07_193027_create_sent_emails_Url_Clicked_table.php' => database_path('migrations/2016_09_07_193027_create_sent_emails_Url_Clicked_table.php')
             ], 'config');
+            $this->publishes([
+                __DIR__.'/../migrations/2016_11_10_213551_add-message-id-to-sent-emails-table.php' => database_path('migrations/2016_11_10_213551_add-message-id-to-sent-emails-table.php')
+            ], 'config');
         }
     }
 
